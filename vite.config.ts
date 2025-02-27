82491226ab8fce35dcd1cee209274d43803ad0ac
tooroot-git <cornpopcorn@duck.com>
2025-02-27 15:09:51 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import runtimeErrorOverlay from '@replit/vite-plugin-runtime-error-modal';
 import themePlugin from '@replit/vite-plugin-shadcn-theme-json';
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
